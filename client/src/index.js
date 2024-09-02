1348d75bdcbe6e4cef0719a0e085d15d1fcc91e9
BenAmaraYoussra <benamarayousra3@gmail.com>
2024-09-02 14:03:36 +0200

    test

---
+++ client/src/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./styles/index.scss";
+import { Provider } from "react-redux";
+import { applyMiddleware, createStore } from "redux";
+import { getPosts } from "./JS/actions/post.actions";
+import { BrowserRouter } from "react-router-dom";
+// import { createEpicMiddleware } from 'redux-observable';
+import rootReducer from "./JS/reducers";
+import { getUsers } from "./JS/actions/users.actions";
+// dev tools
+import { composeWithDevTools } from "redux-devtools-extension";
+import { thunk } from "redux-thunk";
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
+
+store.dispatch(getUsers());
+store.dispatch(getPosts());
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+  // </React.StrictMode>
+);
